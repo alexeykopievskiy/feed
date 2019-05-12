9298232f4591657189cd9907c23a05fcaea85358
alexeykopievskiy <alexeykopievskiy@gmail.com>
2019-05-12 13:01:04 +0300

    done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,6 +10,7 @@ import Button from '../button';
 import Icon from '../icon';
 
 import avatar from '../../assets/avatar-profile.jpg';
+import logoM from '../../assets/lava-m.png';
 
 class Header extends React.PureComponent {
   constructor(props) {
@@ -34,12 +35,72 @@ class Header extends React.PureComponent {
           <div className={styles['header-collapse']}>
             <div className={styles['header-collapse-container']}>
               <div className={styles['header-collapse-head']}>
-                <Icon
-                  name="cross"
-                  onClick={this.onMenuOpen}
-                  size={22}
-                  color="#d2d2db"
-                />
+                <div className={styles['header-collapse-line']}>
+                  <div className={styles['header-collapse-user']}>
+                    <div className={styles['header-collapse-avatar']}>
+                      <img src={avatar} />
+                    </div>
+                    <p>Bruno montero</p>
+                  </div>
+                  <Icon
+                    className={styles['header-collapse-icon']}
+                    name="cross"
+                    onClick={this.onMenuOpen}
+                    size={22}
+                    color="#d2d2db"
+                  />
+                </div>
+                <div className={styles['popup-author-badges']}>
+                  <Button theme="null" className={styles['badge-btn']}>
+                    <div className={styles['badge']}>
+                      <Icon
+                        className={styles['icon-tokens']}
+                        name="tokens"
+                        size={14}
+                      />
+                      <span className={styles['badge-text']}>$ 956</span>
+                    </div>
+                  </Button>
+                  <Button theme="null" className={styles['badge-btn']}>
+                    <div className={styles['badge']}>
+                      <Icon
+                        className={styles['icon-like']}
+                        name="like"
+                        size={14}
+                      />
+                      <span className={styles['badge-text']}>511</span>
+                    </div>
+                  </Button>
+                  <Button theme="null" className={styles['badge-btn']}>
+                    <div className={styles['badge']}>
+                      <Icon
+                        className={styles['icon-like']}
+                        name="followers"
+                        size={14}
+                      />
+                      <span className={styles['badge-text']}>1511</span>
+                    </div>
+                  </Button>
+                </div>
+              </div>
+              <div className={styles['header-collapse-content']}>
+                <div className={styles['nav-mobile']}>
+                  <Link className={styles['nav-item']} to="">
+                    Feed
+                  </Link>
+                  <Link className={styles['nav-item']} to="">
+                    Explore
+                  </Link>
+                  <Link className={styles['nav-item']} to="">
+                    ArtFeed
+                  </Link>
+                  <Link className={styles['nav-item']} to="">
+                    ArtBlog
+                  </Link>
+                </div>
+                <Button className={styles['button-create--mobile']}>
+                  Create project
+                </Button>
               </div>
             </div>
           </div>
@@ -48,6 +109,7 @@ class Header extends React.PureComponent {
           <Icon name="menu" size={25} onClick={this.onMenuOpen} />
           <Link to="">
             <Logo className={styles['logo']} />
+            <img src={logoM}  className={styles['logo--mobile']} />
           </Link>
           <div className={styles['nav']}>
             <Link className={styles['nav-item']} to="">
