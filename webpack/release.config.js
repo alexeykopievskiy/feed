8bc3297ad6d7908b888ff922cb7d98fbf8c79eb9
alexeykopievskiy <alexeykopievskiy@gmail.com>
2019-05-14 20:07:46 +0300

    fix

--- webpack/release.config.js
+++ webpack/release.config.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack');
+/*const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const pkg = require('../package.json');
 const cfg = require('./webpack.config');
@@ -50,4 +50,53 @@ function release(config) {
   });
 }
 
+module.exports = release(cfg);*/
+const webpack = require('webpack');
+const pkg = require('../package.json');
+const cfg = require('./webpack.config');
+
+const cssConfig = {
+  sourceMap: true,
+  modules: true,
+  localIdentName: '[folder]__[local]--[hash:base64:5]',
+};
+
+function release(config) {
+  const plugins = [new webpack.HotModuleReplacementPlugin()];
+
+  plugins.push(
+    ...config.plugins,
+    new webpack.DefinePlugin({ PROJECT_ENV: JSON.stringify('dev') }),
+    new webpack.BannerPlugin(`${pkg.name}-${new Date()}. DEV`)
+  );
+
+  const rules = [...config.module.rules];
+
+  rules.push(
+    {
+      test: /\.inline\.s?css$/,
+      loader: 'style-loader!css-loader!postcss-loader',
+    },
+    {
+      test: /\.s?css$/,
+      exclude: [/node_modules/, /\.inline\.s?css$/],
+      loaders: [
+        'style-loader',
+        `css-loader?${JSON.stringify(cssConfig)}`,
+        'postcss-loader',
+      ],
+    }
+  );
+
+  return Object.assign({}, config, {
+    entry: { index: ['webpack-hot-middleware/client', config.entry.index] },
+    devtool: 'eval-source-map',
+    mode: 'development',
+    watchOptions: { aggregateTimeout: 300 },
+    module: { rules },
+    plugins,
+  });
+}
+
 module.exports = release(cfg);
+
